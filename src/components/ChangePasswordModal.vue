9e225c4b99a085e81324183943d3486ceba031f7
Jungmo Kang <kang.jungmo@abeja.asia>
2018-08-26 10:10:52 +0900

    password display change

--- src/components/ChangePasswordModal.vue
+++ src/components/ChangePasswordModal.vue
@@ -5,14 +5,27 @@
         <p class="modal-card-title">Change Password</p>
       </header>
     <section class="modal-card-body">
-    <b-field label="Password">
-      <b-input
-        type="password"
-        placeholder="Input new password"
-        v-model="newPassword"
-        required>
-      </b-input>
-    </b-field>
+      <div class="field">
+        <label class="label">
+          Password
+        </label>
+        <div class="control is-clearfix has-icons-right">
+          <input
+            class="input"
+            autocomplete="on"
+            :type="passwordField"
+            placeholder="Input new password"
+            v-model="newPassword"
+            required
+          />
+          <b-icon
+            @click.native="changePasswordStatus"
+            class="is-right has-text-info is-clickable"
+            :icon="passwordIcon"
+            size="is-small">
+          </b-icon>
+        </div>
+      </div>
     </section>
     <footer class="modal-card-foot">
       <button class="button" type="button" @click="$parent.close()">Close</button>
@@ -23,16 +36,40 @@
 </template>
 
 <script>
+import zxcvbn from 'zxcvbn'
+
+const PASSWORD_INPUT_TYPE = 'password'
+const TEXT_INPUT_TYPE = 'text'
+
 export default {
   data () {
     return {
-      newPassword: ''
+      newPassword: '',
+      passwordField: PASSWORD_INPUT_TYPE
+    }
+  },
+  computed: {
+    passwordIcon () {
+      return this.passwordField === PASSWORD_INPUT_TYPE ? 'eye' : 'eye-slash'
+    }
+  },
+  watch: {
+    newPassword (changePassword) {
+      console.log(zxcvbn(changePassword))
     }
   },
   methods: {
     savePassword () {
       this.$emit('changePassword', this.newPassword)
       this.$parent.close()
+    },
+    changePasswordStatus () {
+      console.log('changePasswordStatus')
+      if (this.passwordField === PASSWORD_INPUT_TYPE) {
+        this.passwordField = TEXT_INPUT_TYPE
+      } else {
+        this.passwordField = PASSWORD_INPUT_TYPE
+      }
     }
   }
 }
