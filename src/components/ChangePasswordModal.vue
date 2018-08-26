679581b2aa375ba65d9e4b7750d07e10bde8fa20
Jungmo Kang <kang.jungmo@abeja.asia>
2018-08-26 11:02:38 +0900

    add password strength

--- src/components/ChangePasswordModal.vue
+++ src/components/ChangePasswordModal.vue
@@ -24,6 +24,15 @@
             :icon="passwordIcon"
             size="is-small">
           </b-icon>
+          <div v-if="newPassword" class="password-meter">
+            <svg viewBox="0 0 123 2" class="password-meter__bar" preserveAspectRatio="none">
+              <path v-for="(n,index) in 4" :key="index"
+                :d="getActivePath(index)"
+                :class="strengthClass(index)"
+              ></path>
+            </svg>
+            <div class="help is-pulled-right" :class="strengthMessageAndClass.class">{{strengthMessageAndClass.message}}</div>
+          </div>
         </div>
       </div>
     </section>
@@ -40,22 +49,47 @@ import zxcvbn from 'zxcvbn'
 
 const PASSWORD_INPUT_TYPE = 'password'
 const TEXT_INPUT_TYPE = 'text'
-
+const STRENGTH_CLASSE_AND_MSG = [
+  {
+    class: 'password-meter__bar__very-weak',
+    message: 'Very Weak'
+  },
+  {
+    class: 'password-meter__bar__weak',
+    message: 'Weak'
+  },
+  {
+    class: 'password-meter__bar__medium',
+    message: 'Medium'
+  },
+  {
+    class: 'password-meter__bar__strong',
+    message: 'strong'
+  },
+  {
+    class: 'password-meter__bar__very-strong',
+    message: 'Very Strong'
+  }
+]
 export default {
   data () {
     return {
       newPassword: '',
-      passwordField: PASSWORD_INPUT_TYPE
+      passwordField: PASSWORD_INPUT_TYPE,
+      passwordStrength: 0
     }
   },
   computed: {
     passwordIcon () {
       return this.passwordField === PASSWORD_INPUT_TYPE ? 'eye' : 'eye-slash'
+    },
+    strengthMessageAndClass () {
+      return STRENGTH_CLASSE_AND_MSG[this.passwordStrength]
     }
   },
   watch: {
     newPassword (changePassword) {
-      console.log(zxcvbn(changePassword))
+      this.passwordStrength = zxcvbn(changePassword).score
     }
   },
   methods: {
@@ -64,12 +98,23 @@ export default {
       this.$parent.close()
     },
     changePasswordStatus () {
-      console.log('changePasswordStatus')
       if (this.passwordField === PASSWORD_INPUT_TYPE) {
         this.passwordField = TEXT_INPUT_TYPE
       } else {
         this.passwordField = PASSWORD_INPUT_TYPE
       }
+    },
+    getActivePath (index) {
+      /*
+      display strength bar like
+      ------ ------ ------ ------
+      */
+      const offset = 30
+      return `M${offset * index + index} 1 L${offset * (index + 1) + index} 1`
+    },
+    strengthClass (index) {
+      if (this.passwordStrength <= index) return ''
+      return STRENGTH_CLASSE_AND_MSG[this.passwordStrength].class
     }
   }
 }
@@ -78,5 +123,32 @@ export default {
 <style scoped lang='scss'>
 .modal-card-foot {
   justify-content: flex-end;
+}
+.password-meter {
+  .password-meter__bar {
+    color: rgb(204, 204, 204);
+    display: block;
+    height: .5rem;
+    margin-top: .2rem;
+    padding-left: .5rem;
+    padding-right: .5rem;
+    width: 100%;
+    path {
+      stroke: currentColor;
+      stroke-width: 2;
+    }
+  }
+  .password-meter__bar__weak {
+    color: rgb(255, 0, 0)
+  }
+  .password-meter__bar__medium {
+    color: rgb(255, 160, 65);
+  }
+  .password-meter__bar__strong {
+    color: rgb(93, 185, 70);
+  }
+  .password-meter__bar__very-strong {
+    color: rgb(25, 0, 255);
+  }
 }
 </style>
