ccca2663bcfb4b3850b553635fbec66aae265456
Jungmo Kang <kang.jungmo@abeja.asia>
2018-08-26 11:29:39 +0900

    add back button

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
+    <router-link :to="{ name: 'UserList' }">
+      <outline-button icon="chevron-left" title="Back"/>
+    </router-link>
     <user-profile :user="userById($route.params.id)"/>
   </div>
 </template>
 
 <script>
+import OutlineButton from '../components/common/OutlineButton'
 import { mapGetters } from 'vuex'
 import UserProfile from '@/components/UserProfile'
 export default {
   components: {
-    UserProfile
+    UserProfile,
+    OutlineButton
   },
   computed: {
     ...mapGetters({
